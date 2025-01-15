7655f16eaf55be109fa5cfc906db170eac6f1e8a
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-01-15 19:36:27 +0600

    feat(dev) - setting up request object to work with it

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
 
 // dependencies
 const http = require('http');
+const url = require('url');
+const { StringDecoder } = require('string_decoder');
 
 // app object - module scafoldiing
 const app = {};
@@ -20,14 +22,35 @@ app.config = {
 app.createServer = () => {
     const server = http.createServer(app.handleReqRes);
     server.listen(app.config.port, () => {
-        console.log(`listening on port ${app.config.port}`);
+        console.log(`listening on port ${app.config.port}.....`);
     });
 };
 
 // handle request response
 app.handleReqRes = (req, res) => {
-    // response handle
-    res.end('Hello Programmers!');
+    // request handle
+    // get the url and parse it
+    const parsedurl = url.parse(req.url);
+    const path = parsedurl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedurl.query;
+    const headersObject = req.headers;
+
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    });
+
+    req.on('end', () => {
+        realData += decoder.end();
+
+        console.log(realData);
+        // response handle
+        res.end('Hello Programmers!');
+    });
 };
 
 // start the server
